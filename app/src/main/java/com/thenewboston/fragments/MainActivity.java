5ccdb84f44a1a9e52154ed0f9dc912dc8461dfa5
Yo-Yo <yofu1234@gmail.com>
2015-06-26 23:06:01 -0700

    Initial commit

---
+++ app/src/main/java/com/thenewboston/fragments/MainActivity.java
@@ -0,0 +1,47 @@
+//Fragments are essential. They are part of your activity that you can reuse over and over again in other activities in your app. IE: menu bar
+
+package com.thenewboston.fragments;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity implements TopSectionFragment.TopSectionListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    //This gets called by the Top Fragment when the user clicks the button
+    @Override
+    public void createMeme(String top, String bottom) {
+        BottomPictureFragment bottomFragment = (BottomPictureFragment) getSupportFragmentManager().findFragmentById(R.id.fragment2);
+        bottomFragment.setMemeText(top, bottom);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
